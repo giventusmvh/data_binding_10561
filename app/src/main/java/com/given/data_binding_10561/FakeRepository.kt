ab11f172a4823db01f2b698f6582c88202fe60c4
giventusmvh <weishen735@gmail.com>
2022-09-15 13:56:30 +0700

    Initial commit

---
+++ app/src/main/java/com/given/data_binding_10561/FakeRepository.kt
@@ -0,0 +1,30 @@
+package com.given.data_binding_10561
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlin.random.Random
+
+object FakeRepository {
+    private val fruitNames: List<String> = listOf(
+        "Apel","Mangga","Pisang","Anggur","Stroberri",
+        "Jambu","Raspberi","Bambu","Kiwi","Pir"
+    )
+
+    private val _currentRandomFruitName = MutableLiveData<String>()
+    val currentRandomFruitName: LiveData<String>
+        get() = _currentRandomFruitName
+
+    init{
+        _currentRandomFruitName.value = fruitNames.first()
+    }
+
+    fun getRandomfruitName():String{
+        val random = Random.nextInt(fruitNames.size)
+        return fruitNames[random]
+    }
+
+    fun changeCurrentRandomFruitName(){
+        _currentRandomFruitName.value = getRandomfruitName()
+    }
+
+}
---
+++ app/src/main/java/com/given/data_binding_10561/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.given.data_binding_10561
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val currentRandomFruitName: LiveData<String>
+    get() = FakeRepository.currentRandomFruitName
+
+    fun onChangeRandomFruitClick()=FakeRepository.changeCurrentRandomFruitName()
+
+    val editTextContent = MutableLiveData<String>()
+    private val _displayedEditTextContent=MutableLiveData<String>()
+    val displayedEditTextContent:LiveData<String>
+    get() = _displayedEditTextContent
+
+    fun onDisplayEditTextContentClick(){
+        _displayedEditTextContent.value=editTextContent.value
+    }
+
+    fun onSelectRandomEditTextFruit(){
+        editTextContent.value=FakeRepository.getRandomfruitName()
+    }
+}
